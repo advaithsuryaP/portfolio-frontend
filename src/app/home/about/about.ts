8d965b46ac5bac218644491fe1caa397c90b7b1e
advaithsuryaP <padvaithsurya@gmail.com>
2025-06-22 18:08:18 -0400

    Add Firebase configuration and new components for personal and professional sections
    - Created .firebaserc and firebase.json for Firebase hosting setup.
    - Added new components for personal and professional sections with corresponding HTML, SCSS, and TypeScript files.
    - Updated about component to include a toggle for personal view.
    - Adjusted styles for better layout and responsiveness.

--- src/app/home/about/about.ts
+++ src/app/home/about/about.ts
@@ -1,9 +1,14 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 
 @Component({
     selector: 'app-about',
-    imports: [],
+    imports: [FormsModule, MatIconModule, MatSlideToggleModule],
     templateUrl: './about.html',
-    styleUrl: './about.scss',
+    styleUrl: './about.scss'
 })
-export default class About {}
+export default class About {
+    showPersonalView: boolean = false;
+}
