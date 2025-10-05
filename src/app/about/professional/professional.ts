dd2d26f30e69c71ca3bf155c905a07f61e0fac26
advaithsuryaP <padvaithsurya@gmail.com>
2025-10-05 18:03:58 -0400

    Compartmentalize professional section

--- src/app/about/professional/professional.ts
+++ src/app/about/professional/professional.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatDividerModule } from '@angular/material/divider';
+import { Experience } from './experience/experience';
+import { Education } from './education/education';
+import { Skills } from './skills/skills';
+import { Summary } from './summary/summary';
 
 @Component({
     selector: 'app-professional',
-    imports: [MatIconModule, MatDividerModule],
+    imports: [MatIconModule, MatDividerModule, Experience, Education, Skills, Summary],
     templateUrl: './professional.html',
     styleUrl: './professional.scss'
 })
