1314dca60ab85721ec4139f1fe434d19cb708acf
advaithsuryaP <padvaithsurya@gmail.com>
2025-10-06 13:36:58 -0400

    Add images to companies

--- src/app/about/professional/experience/experience.ts
+++ src/app/about/professional/experience/experience.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import Icon from '../../../ui/icon/icon';
 
 @Component({
     selector: 'app-experience',
-    imports: [MatIconModule, MatButtonModule],
+    imports: [MatIconModule, MatButtonModule, Icon],
     templateUrl: './experience.html',
     styleUrl: './experience.scss'
 })
